dcf7a514cc52fb5da8679b7ba072fe1c26aff01a
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-03-01 16:41:20 +0100

    Performance: removed blocking JavaScript and optimized main thread work

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 // Simulerad blockering av tråden
-setInterval(() => {
-  let start = Date.now()
-  while (Date.now() - start < 1000) {}
-}, 3000)
+function runForAbout(ms) {
+  const endTime = performance.now() + ms;
 
-// Skriver ut HTML direkt i dokumentet
-document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>")
-document.write("<p>Another document.write() call</p>")
-
-// Förhindrar användaren att navigera bakåt
-history.pushState(null, "", location.href)
-window.onpopstate = () => history.go(1)
-
-// Ändrar bakgrundsfärgen slumpmässigt var 500ms
-setInterval(() => {
-  document.body.style.backgroundColor =
-    "#" + Math.floor(Math.random() * 16777215).toString(16)
-}, 500)
-
-// Visar innehållet på sidan efter 5 sekunder
-setTimeout(() => {
-  document.body.style.display = "block"
-}, 5000)
-
-// Loggar 10000 tal till konsolen efter 1 sekund
-setTimeout(() => {
-  for (let i = 0; i < 10000; i++) {
-    console.log(i)
+  function step() {
+    for (let i = 0; i < 2000; i++) {}
+    if (performance.now() < endTime) {
+      requestAnimationFrame(step);
+    }
   }
-}, 1000)
-
-// // Visar en alert när sidan har laddats
-// document.addEventListener("DOMContentLoaded", function () {
-//   alert("Page loaded!")
-// })
-
-// Loggar 100 000 siffror direkt när sidan laddas
-for (let i = 0; i < 100000; i++) {
-  console.log(i)
+  requestAnimationFrame(step);
 }
+
+setInterval(() => {
+  runForAbout(300);
+}, 3000);
+
+// Byt bakgrund vid klick istället
+document.body.addEventListener("click", () => {
+  document.body.style.backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+});
+
+
+
+
+
+
+
+
